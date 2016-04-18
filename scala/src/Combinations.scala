e85ab0262e53e5361c951cef47e4a1226247a51b
jeff-sweeney <jsweeney@uchicago.edu>
2016-04-18 13:38:42 -0500

    tail recursive factorial

--- scala/src/Combinations.scala
+++ scala/src/Combinations.scala
@@ -1,4 +1,6 @@
 
+import scala.annotation._
+
 /**
  * Form all possible combinations of n mutually distinguishable
  * elements m at a time where each combination is a selection of m
@@ -18,21 +20,6 @@ object Combinations {
 
     // t is total number of nonrepeating combinations: 2^n
     val t = BigInt( 1 ) << n
-
-
-    // bimonial coefficients for all m
-    println("binomial coefficients without factorials, m=0 to m=n:")
-    for ( m <- 0 to n ) {
-      println( binomialCoefficient( n, m ) )
-    }
-    println("")
-
-    println("binomial coefficients with factorials, m=0 to m=n:")
-    for ( m <- 0 to n ) {
-      println( binomialCoefficientFactorial( n, m ) )
-    }
-    println("")
-
 
     // human order for all m
     println("human order, m=0 to m=n:")
@@ -68,7 +55,7 @@ object Combinations {
      */
     private def humanOrder( elements: Array[String], n: Int, t: BigInt, m: Int ) {
       // total number of combinations with m elements
-      val b = binomialCoefficientFactorial( n, m )
+      val b = binomialCoefficient( n, m )
       // current count of combinations with m elements
       var q = BigInt( 0 )
 
@@ -100,58 +87,56 @@ object Combinations {
       }
     }
 
-    
-    /** machine order for all m
-      *
-      * @param elements The set of elements
-     * @param n The total number of elements
-     * @param t The total number of nonrepeating combinations
-     */
-    private def machineOrder( elements: Array[String], n: Int, t: BigInt ) {
-        // Iterate through all combinations of nonrepeating elements, incrementing by one.
-      var i = BigInt( 0 )
-      while ( i != t ) {
-        val combination = new Array[String]( n )
-        val empty = "-"
+
+  /** machine order for all m. Recurse through all combinations of nonrepeating elements.
+    * @param elements The set of elements
+    * @param n The total number of elements
+    * @param t The total number of nonrepeating combinations
+    * @param accumulator The current number of nonrepeating combinations
+    */
+  @tailrec
+  private def machineOrder( elements: Array[String], n: Int, t: BigInt, accumulator: BigInt = 0 ) {
+    if ( accumulator != t ) {
+      val combination = new Array[String]( n )
+      val empty = "-"
                     
-        // For all n elements test whether element j is present.
-        for ( j <- 0 until n ) {
-          // If present add it to the current combination.
-          if ( i.testBit( j ) )
-            combination( j ) = elements( j )
-          else
-            combination( j ) = empty
-        }
-
-        // List the current combination.
-        listCombination( combination )
-        i = i + 1
+      // For all n elements test whether element j is present.
+      for ( j <- 0 until n ) {
+        // If present add it to the current combination.
+        if ( accumulator.testBit( j ) )
+          combination( j ) = elements( j )
+        else
+          combination( j ) = empty
       }
+
+      // List the current combination.
+      listCombination( combination )
+      machineOrder( elements, n, t, accumulator + 1 )
     }
+  }
 
+  /** machine order for a specific m
+    *
+    * @param elements The set of elements
+    * @param n The total number of elements
+    * @param t The total number of nonrepeating combinations
+    * @param m The number of elements to be combined
+    */
+  private def machineOrder( elements: Array[String], n: Int, t: BigInt, m: Int ) {
+    // total number of combinations with m elements
+    val b = binomialCoefficient( n, m )
 
-    /** machine order for a specific m
-      *
-      * @param elements The set of elements
-     * @param n The total number of elements
-     * @param t The total number of nonrepeating combinations
-     * @param m The number of elements to be combined
-     */
-    private def machineOrder( elements: Array[String], n: Int, t: BigInt, m: Int ) {
-      // total number of combinations with m elements
-      val b = binomialCoefficientFactorial( n, m )
-      // current count of combinations with m elements
-      var q = BigInt( 0 )
+    // current count of combinations with m elements
+    var q = BigInt( 0 )
 
-      // Iterate through all combinations of nonrepeating elements, incrementing by one.
+    // Iterate through all combinations of nonrepeating elements, incrementing by one.
       var i = BigInt( 0 )
-      while ( i != t && q != b ) {
-        if ( i.bitCount == m )
-        {
+      while ( i < t && q < b ) {
+        if ( i.bitCount == m ) {
           q = q + 1
           val combination = new Array[String]( n )
           val empty = "-"
-                    
+
           // For all n elements test whether element j is present.
           for ( j <- 0 until n ) {
             // If present add it to the current combination.
@@ -165,38 +150,23 @@ object Combinations {
           listCombination( combination )
         }
         i = i + 1
-      }
     }
-
-
-  /** Binomial coefficient used for optimization with large n
-    * @param n The total number of elements
-    * @param m The number of elements to be combined
-    */
-  private def binomialCoefficient( n: Int, m: Int ): BigInt = {
-    val b = new Array[BigInt]( n + 1 )
-    b( 0 ) = 1
-    for ( i <- 1 to n ) {
-      b( i ) = 1
-      for ( j <- i-1 until 0 by -1 )
-        b( j ) =  b( j ) + b( j - 1 )
-    }
-
-    b( m )
   }
 
-  /** Binomial coefficient used for optimization with large n using factorials
+
+  /** Binomial coefficient for optimization with large n. Uses factorials.
     * @param n The total number of elements
     * @param m The number of elements to be combined
     */
-  private def binomialCoefficientFactorial( n: Int, m: Int ): BigInt =
+  private def binomialCoefficient( n: Int, m: Int ): BigInt =
     factorial( n ) / ( factorial( m ) * factorial( n - m ) )
 
   /** Factorial used for binomial coefficient
-    *  @param n The number for which the facorial is calculated
+    *  @param n The number for which the factorial is calculated
     */
-  private def factorial( n: Int ): BigInt =
-    if ( n == 0 ) 1 else n * factorial( n - 1 )
+  @tailrec
+  private def factorial( n: Int, accumulator: BigInt = 1 ): BigInt =
+    if ( n == 0 ) accumulator else factorial( n - 1, accumulator * n )
 
   /** Process the given combination. Here we just write it to the console.
     *
